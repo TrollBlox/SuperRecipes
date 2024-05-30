7b91caee49addb103dbe9b158bebb07864c5d04e
TrollBlox <caelbrenning@gmail.com>
2024-05-29 21:59:45 -0500

    i wish this is how i could implement it but no worky. just pushing to save it

---
+++ src/main/java/net/trollblox/superrecipes/datagen/SuperRecipesRecipeProvider.java
@@ -0,0 +1,61 @@
+package net.trollblox.superrecipes.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.Identifier;
+
+import java.util.concurrent.CompletableFuture;
+
+public class SuperRecipesRecipeProvider extends FabricRecipeProvider {
+    public SuperRecipesRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    public void generate(RecipeExporter exporter) {
+        // Crossbow Dispenser Recipe
+        ShapedRecipeJsonBuilder.create(RecipeCategory.REDSTONE, Items.DISPENSER, 1)
+                .pattern("###")
+                .pattern("#C#")
+                .pattern("#D#")
+                .input('#', Items.COBBLESTONE).input('D', Items.REDSTONE).input('C', Items.CROSSBOW)
+                .criterion(hasItem(Items.REDSTONE), conditionsFromItem(Items.REDSTONE))
+                .offerTo(exporter, new Identifier(getRecipeName(Items.DISPENSER)));
+        generateMusicDiscRecipes(exporter);
+    }
+
+    private static void generateMusicDiscRecipes(RecipeExporter exporter) {
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_5);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_11);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_13);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_CAT);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_CHIRP);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_BLOCKS);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_FAR);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_MALL);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_MELLOHI);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_OTHERSIDE);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_PIGSTEP);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_RELIC);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_STAL);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_STRAD);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_WAIT);
+        offerMusicDiscDuplicationRecipe(exporter, Items.MUSIC_DISC_WARD);
+    }
+
+    public static void offerMusicDiscDuplicationRecipe(RecipeExporter exporter, ItemConvertible item) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, item, 1 )
+                .pattern("###")
+                .pattern("#X#")
+                .pattern("###")
+                .input('#', Items.DIAMOND).input('X', item)
+                .criterion(hasItem(item), conditionsFromItem(item))
+                .offerTo(exporter, new Identifier(getRecipeName(item)));
+    }
+}
---
+++ src/main/java/net/trollblox/superrecipes/mixin/MusicDiscRemainderMixin.java
@@ -0,0 +1,25 @@
+package net.trollblox.superrecipes.mixin;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.MusicDiscItem;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Debug(export = true)
+@Mixin(Item.class)
+public class MusicDiscRemainderMixin {
+
+    @Shadow
+    private Item recipeRemainder;
+    @Inject(at = @At("TAIL"), method = "<init>")
+    private void overrideSetRecipeRemainder(CallbackInfo info) {
+        if ((Object) this instanceof MusicDiscItem) {
+            recipeRemainder = ((MusicDiscItem) (Object) this);
+        }
+    }
+
+}
