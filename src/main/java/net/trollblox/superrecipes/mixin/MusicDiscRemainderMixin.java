d4465a1623f0ab0e21dc9003c9b7a8c0b1a3e6c0
TrollBlox <caelbrenning@gmail.com>
2024-06-02 17:06:27 -0500

    refactored code to be more compatible with other mods

--- src/main/java/net/trollblox/superrecipes/mixin/MusicDiscRemainderMixin.java
+++ src/main/java/net/trollblox/superrecipes/mixin/MusicDiscRemainderMixin.java
@@ -2,23 +2,26 @@ package net.trollblox.superrecipes.mixin;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.MusicDiscItem;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Debug(export = true)
 @Mixin(Item.class)
 public class MusicDiscRemainderMixin {
-    @Inject(at = @At("HEAD"), method = "getRecipeRemainder", cancellable = true)
-    protected void overrideGetRecipeRemainder(CallbackInfoReturnable<Item> info) {
+
+    @Mutable
+    @Shadow
+    private Item recipeRemainder;
+    @Inject(at = @At("TAIL"), method = "<init>")
+    private void overrideSetRecipeRemainder(CallbackInfo info) {
         if ((Object) this instanceof MusicDiscItem) {
-            info.setReturnValue((MusicDiscItem) (Object)this);
+            recipeRemainder = ((MusicDiscItem) (Object) this);
         }
     }
-    @Inject(at = @At("HEAD"), method = "hasRecipeRemainder", cancellable = true)
-    protected void overrideHasRecipeRemainder(CallbackInfoReturnable<Boolean> info) {
-        if ((Object) this instanceof MusicDiscItem) {
-            info.setReturnValue(true);
-        }
-    }
+
 }
