6370b3d0321e8b3c429bc085ff6d34b443c7a536
TrollBlox <caelbrenning@gmail.com>
2024-06-04 21:56:18 -0500

    changed modid

--- src/main/java/net/trollblox/superrecipes/SuperRecipes.java
+++ src/main/java/net/trollblox/superrecipes/SuperRecipes.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SuperRecipes implements ModInitializer {
-	public static final String MOD_ID = "superrecipes";
+	public static final String MOD_ID = "super-recipes";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
