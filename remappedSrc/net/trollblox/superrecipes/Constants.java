8dee5d888a02887158a85a27ad6d5ca87b316b06
Cael B <caelbrenning@gmail.com>
2024-09-30 20:03:52 -0500

    idk what remappedSrc is, gone now

--- remappedSrc/net/trollblox/superrecipes/Constants.java
+++
@@ -1,9 +0,0 @@
-package net.trollblox.superrecipes;
-
-public class Constants {
-    public static final String MOD_ID = "super-recipes";
-
-    public static final String HOPPER_SPEED_NBT_ID = "HopperSpeed";
-    @Deprecated
-    public static final String HOPPER_SPEED_NBT_ID_OLD = "SlowHopper";
-}
--- remappedSrc/net/trollblox/superrecipes/HopperSpeedData.java
+++
@@ -1,6 +0,0 @@
-package net.trollblox.superrecipes;
-
-public interface HopperSpeedData {
-    void super_recipes_1_21$setHopperSpeed(net.trollblox.superrecipes.enums.HopperSpeed hopperSpeed);
-    net.trollblox.superrecipes.enums.HopperSpeed super_recipes_1_21$getHopperSpeed();
-}
